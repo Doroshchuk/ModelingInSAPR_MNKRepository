101dd7ca71ff3ef67c020e5f586708ec04203846
ddoroshchuk <doroshukdashuly@mail.ru>
2018-03-19 12:17:03 +0200

    two classes Interface, Plot are modified, class Rescale is added: 1.partially scaled, 2.the possibility to set points from the keyboard, and with the mouse, 3.the possibility to adjust points (add, delete), 4.the possibility to clear the screen.

--- src/Interface.java
+++ src/Interface.java
@@ -1,10 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -12,7 +9,7 @@ public class Interface {
     private JPanel inputPanel, drawingPanel;
     private JFrame frame;
     private Plot plot;
-    private Font font = new Font("Arial", Font.PLAIN, 10);
+    private Font font = new Font("TimesRoman", Font.PLAIN, 10);
 
     public static void main(String[] args) {
         new Interface();
@@ -45,6 +42,58 @@ public class Interface {
         setJPanels(inputPanel, drawingPanel);
         plot = new Plot();
         drawingPanel.add(plot);
+
+        JLabel lbl = createLabel("Укажите координаты новой точки: ", new int[]{30, 20, 190, 20}, inputPanel);
+
+        createLabel("x = : ", new int[]{80, 50, 40, 20}, inputPanel);
+        JTextField coordinateOfXTF = createTextField(new int[]{100, 50, 60, 20}, "", inputPanel);
+
+        createLabel("y = : ", new int[]{80, 80, 40, 20}, inputPanel);
+        JTextField coordinateOfYTF = createTextField(new int[]{100, 80, 60, 20}, "", inputPanel);
+
+        JLabel messageLbl = createLabel("", new int[]{28, 150, 200, 40}, inputPanel);
+
+        JButton addNewPointBtn = createButton(new int[]{40, 110, 40, 40}, "Images/executing.png", inputPanel, (ActionEvent event) -> {
+            double x = Double.parseDouble(coordinateOfXTF.getText());
+            double y = Double.parseDouble(coordinateOfYTF.getText());
+
+            Point newPoint = new Point(x, y);
+            if(plot.getPoints().contains(newPoint)) {
+                repaintSelectedPoint(newPoint);
+            } else addNewPointAndRepaint(newPoint);
+        });
+
+        JButton removeSelectedPointBtn = createButton(new int[]{100, 110, 40, 40}, "Images/executing.png", inputPanel, (ActionEvent event) -> {
+            if(!(plot.getSelectedPoint() == null)){
+                drawingPanel.remove(plot);
+                plot.removeSelectedPoint();
+                repaintDrawingPanel(plot);
+            } else {
+                String messageText = "Вы не выбрали точку для удаления.";
+                messageLbl.setText("<html><div style='text-align: center;'>" + messageText + "</div></html>");
+            }
+        });
+
+        JButton removeAllPointsBtn = createButton(new int[]{160, 110, 40, 40}, "Images/executing.png", inputPanel, (ActionEvent event) -> {
+            drawingPanel.remove(plot);
+            plot.getPoints().clear();
+            plot.setChoice(false);
+            repaintDrawingPanel(plot);
+        });
+
+        JCheckBox checkBox = new JCheckBox();
+        checkBox.setText("Execute MNK by line");
+        checkBox.addActionListener(e -> {
+            JCheckBox choice = (JCheckBox) e.getSource();
+            if (choice.isSelected()) {
+
+            } else {
+
+            }
+        });
+
+        checkBox.setBounds(40, 10, 120, 20);
+        inputPanel.add(checkBox);
     }
 
     private JTextField createTextField(int bounds[], String value, JPanel panel) {
@@ -85,23 +134,46 @@ public class Interface {
         inputPanel.setLayout(null);
         drawingPanel.setVisible(true);
 
-        JLabel messageLbl = createLabel("", new int[]{20, 120, 200, 40}, inputPanel);
-        messageLbl.setVisible(false);
-
         drawingPanel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                Point newPoint = new Point(e.getX() - drawingPanel.getWidth() / 2, -(e.getY() - drawingPanel.getHeight() / 2));
-                if(plot.getPoints().contains(newPoint)){
-                    messageLbl.setVisible(true);
-                    String messageText = "Точка с координатами (" + newPoint.getX() + ", " + newPoint.getY() + ") уже присутствует.";
-                    messageLbl.setText("<html><div style='text-align: center;'>" + messageText + "</div></html>");
-                }else{
-                    plot.addPoint(newPoint);
-                    for (Point point: plot.getPoints())
-                        System.out.println(point.getX() + " " + point.getY());
-                }
+                selectPoint(e);
             }
         });
+    }
+
+    private void addNewPointAndRepaint(Point point){
+        plot.addPoint(point);
+        plot.setSelectedPoint(null);
+        drawingPanel.remove(plot);
+        plot.setChoice(true);
+    }
+
+    private void selectPoint(MouseEvent e){
+        double distance = Double.MAX_VALUE;
+        Point nearestPoint = null;
+        Point position = new Point(e.getX() - drawingPanel.getWidth() / 2, drawingPanel.getHeight() / 2 - e.getY());
+        for (Point point : plot.getPoints()) {
+            double newDistance = plot.getDistance(point, position);
+            if (newDistance < 5 && newDistance < distance) {
+                nearestPoint = point;
+                distance = newDistance;
+            }
+        }
+        if (distance != Double.MAX_VALUE) {
+            repaintSelectedPoint(nearestPoint);
+        } else addNewPointAndRepaint(position);
+    }
+
+    private void repaintSelectedPoint(Point selectedPoint){
+        drawingPanel.remove(plot);
+        plot.setSelectedPoint(selectedPoint);
+        repaintDrawingPanel(plot);
+    }
+
+    private void repaintDrawingPanel(Plot plot){
+        drawingPanel.add(plot);
+        drawingPanel.validate();
+        drawingPanel.repaint();
     }
 }
--- src/Plot.java
+++ src/Plot.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.regexp.internal.RE;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
@@ -5,10 +7,32 @@ import java.util.ArrayList;
 
 public class Plot extends JPanel {
     private Graphics2D graphics;
-    private double unitVectorSize;
+    private double unitVectorSizeX;
+    private double unitVectorSizeY;
     private double x0;
     private double y0;
     private ArrayList<Point> points;
+    private ArrayList<Line> axisLines;
+    private ArrayList<Line> gridLines;
+    private boolean choice;
+    private Point selectedPoint;
+    private String typeOfMNK;
+
+    public String getTypeOfMNK() {
+        return typeOfMNK;
+    }
+
+    public void setTypeOfMNK(String typeOfMNK) {
+        this.typeOfMNK = typeOfMNK;
+    }
+
+    public Point getSelectedPoint() {
+        return selectedPoint;
+    }
+
+    public void setSelectedPoint(Point selectedPoint) {
+        this.selectedPoint = selectedPoint;
+    }
 
     public ArrayList<Point> getPoints() {
         return points;
@@ -18,12 +42,20 @@ public class Plot extends JPanel {
         this.points = points;
     }
 
-    public double getUnitVectorSize() {
-        return unitVectorSize;
+    public double getUnitVectorSizeX() {
+        return unitVectorSizeX;
     }
 
-    public void setUnitVectorSize(double unitVectorSize) {
-        this.unitVectorSize = unitVectorSize;
+    public void setUnitVectorSizeX(double unitVectorSizeX) {
+        this.unitVectorSizeX = unitVectorSizeX;
+    }
+
+    public double getUnitVectorSizeY() {
+        return unitVectorSizeY;
+    }
+
+    public void setUnitVectorSizeY(double unitVectorSizeY) {
+        this.unitVectorSizeY = unitVectorSizeY;
     }
 
     public double getX0() {
@@ -42,6 +74,14 @@ public class Plot extends JPanel {
         this.y0 = y0;
     }
 
+    public boolean isChoice() {
+        return choice;
+    }
+
+    public void setChoice(boolean choice) {
+        this.choice = choice;
+    }
+
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(1000, 720);
@@ -55,28 +95,46 @@ public class Plot extends JPanel {
         // All coordinates below center canvas are negative
         this.graphics = (Graphics2D) graphics;
         this.graphics.translate(getWidth() / 2, getHeight() / 2);
-        drawAxis();
         drawLinesForCoordinateGrid();
+        graphics.setColor(Color.BLACK);
+        drawAxis();
+        if (choice) zoom();
+        else designateCoordinateAxes();
+        if (!points.isEmpty())
+            drawVertexes(points);
     }
 
     public Plot() {
-        unitVectorSize = 20;
+        unitVectorSizeX = 20;
+        unitVectorSizeY = 20;
         x0 = 0;
         y0 = 0;
         points = new ArrayList<>();
+        axisLines = new ArrayList<>();
+        gridLines = new ArrayList<>();
     }
 
     private void drawLinesForCoordinateGrid() {
-        for (int i = 0; i < getWidth(); i += unitVectorSize) {
+        for (int i = 0; i < getWidth(); i += unitVectorSizeY) {
             //vertical lines
             drawLine(new Point(x0 - getWidth() / 2 + i, y0 + getHeight()), new Point(x0 - getWidth() / 2 + i, y0 - getHeight()), TypeOfLine.COORDINATEGridLine);
         }
-        for (int i = 0; i < getHeight(); i += unitVectorSize) {
+        for (int i = 0; i < getHeight(); i += unitVectorSizeX) {
             //horizontal lines
             drawLine(new Point(x0 - getWidth(), y0 - getHeight() / 2 + i), new Point(x0 + getWidth(), y0 - getHeight() / 2 + i), TypeOfLine.COORDINATEGridLine);
         }
-        drawLine(new Point(unitVectorSize, -2), new Point(unitVectorSize, 2), TypeOfLine.COORDINATEGridLine);
-        drawLine(new Point(-2, unitVectorSize), new Point(2, unitVectorSize), TypeOfLine.COORDINATEGridLine);
+    }
+
+    private void designateCoordinateAxes(){
+        graphics.setFont(new Font("TimesRoman", Font.PLAIN, 11));
+        for (int y = - getHeight() / 2; y < getHeight() / 2; y += unitVectorSizeY){
+            drawLine(new Point(-4, y), new Point(4, y), TypeOfLine.AXISLine);
+            graphics.drawString("" + y, 4, y);
+        }
+        for (int x = - getWidth() / 2; x < getWidth() / 2; x += unitVectorSizeX){
+            drawLine(new Point(x, -4), new Point(x, 4), TypeOfLine.AXISLine);
+            graphics.drawString("" + x, x, 10);
+        }
     }
 
     private void drawAxis() {
@@ -88,15 +146,24 @@ public class Plot extends JPanel {
         drawLine(new Point(-getWidth() / 2, 0), new Point(getWidth() / 2, 0), TypeOfLine.AXISLine);
         drawLine(new Point(getWidth() / 2 - 10, -10), new Point(getWidth() / 2, 0), TypeOfLine.AXISLine);
         drawLine(new Point(getWidth() / 2 - 10, 10), new Point(getWidth() / 2, 0), TypeOfLine.AXISLine);
-        graphics.setColor(Color.BLACK);
-        graphics.setFont(new Font("TimesRoman", Font.PLAIN, 11));
-        graphics.drawString("20", (int) unitVectorSize - 12, 10);
-        graphics.drawString("20", 4, (int) -unitVectorSize);
-        graphics.setFont(new Font("TimesRoman", Font.BOLD, 15));
+        graphics.setFont(new Font("TimesRoman", Font.BOLD, 13));
+        graphics.drawString("y", 20, -getHeight() / 2 + 15);
+        graphics.drawString("x", (int) (getWidth() / 2 - 30), -15);
     }
 
     public void addPoint(Point newPoint){
         points.add(newPoint);
+    }
+
+    private void drawVertexes(ArrayList<Point> points) {
+        int size = 4;
+        for (Point point : points) {
+            if (point.equals(selectedPoint))
+                graphics.setColor(Color.RED);
+            graphics.fillOval((int) (x0 + point.getX() - size / 2), (int) (y0 - point.getY() - size / 2), size, size);
+            graphics.setColor(Color.BLACK);
+            graphics.drawString(Integer.toString(points.indexOf(point)), (int) (x0 + point.getX() + size), (int) (y0 - point.getY()));
+        }
     }
 
     private void drawLine(Point point1, Point point2, TypeOfLine typeOfLine) {
@@ -104,10 +171,14 @@ public class Plot extends JPanel {
             case COORDINATEGridLine:
                 graphics.setStroke(new BasicStroke(1f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
                 graphics.setColor(new Color(0f, 0f, 0f, .4f));
+                if (!gridLines.contains(new Line(point1, point2, typeOfLine)))
+                    gridLines.add(new Line(point1, point2, typeOfLine));
                 break;
             case AXISLine:
                 graphics.setStroke(new BasicStroke(1f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
-                graphics.setColor(new Color(0f, 0f, 0f, .6f));
+                graphics.setColor(new Color(0f, 0f, 0f, .8f));
+                if (!axisLines.contains(new Line(point1, point2, typeOfLine)))
+                    axisLines.add(new Line(point1, point2, typeOfLine));
                 break;
             case CURVELine:
                 graphics.setStroke(new BasicStroke(2f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
@@ -119,5 +190,133 @@ public class Plot extends JPanel {
                 break;
         }
         graphics.draw(new Line2D.Double(point1.getX(), -point1.getY(), point2.getX(), -point2.getY()));
+    }
+
+    public void zoom(){
+        double maxX = findMaxValue("x", points);
+        double maxY = findMaxValue("y", points);
+        double minX = findMinValue("x", points);
+        double minY = findMinValue("y", points);
+
+        Rescale rescaleX = new Rescale(minX, maxX, - getWidth() / 2 + 20, getWidth() / 2 - 20);
+        Rescale rescaleY = new Rescale(minY, maxY, - getHeight() / 2 + 20, getHeight() / 2 - 20);
+
+        for (Point point: points){
+            point.setX(rescaleX.rescale(point.getX()));
+            point.setY(rescaleY.rescale(point.getY()));
+            System.out.println("result: " + point.getX() + "," + point.getY());
+        }
+//        for (double sign = (int) minY, h = - getHeight() / 2; sign <= maxY && h < getHeight() / 2; sign += rescaleY.rescale(unitVectorSizeY), h += unitVectorSizeY){
+//            graphics.drawString("" + (int) sign, 4, (int) - h);
+//            System.out.println(sign + "    " + h);
+//        }
+//        for (int x = - getWidth() / 2; x < getWidth() / 2; x += unitVectorSizeX){
+//            graphics.drawString("" + rescaleX.rescale(x), x, 10);
+//        }
+    }
+//
+//    public void performAffineTransformation(Point r0, Point r_x, Point r_y){
+//        x0 = r0.getX() + r_x.getX() * x0 + r_x.getY() * y0;
+//        y0 = r0.getY() + r_y.getX() * x0 + r_y.getY() * y0;
+//        for (Point point: points){
+//            point.setX(r0.getX() + r_x.getX() * point.getX() + r_x.getY() * point.getY());
+//            point.setY(r0.getY() + r_y.getX() * point.getX() + r_y.getY() * point.getY());
+//        }
+//        axisLines = manipulateAffinnesTransformation(axisLines, r0, r_x, r_y);
+//        gridLines = manipulateAffinnesTransformation(gridLines, r0, r_x, r_y);
+//    }
+//
+//    public ArrayList<Line> manipulateAffinnesTransformation(ArrayList<Line> initialLines, Point r0, Point r_x, Point r_y){
+//        ArrayList<Line> transformedLines = new ArrayList<>();
+//        for (Line initialLine : initialLines) {
+//            double x_st = r0.getX() + r_x.getX() * initialLine.getStartPoint().getX() + r_x.getY() * initialLine.getStartPoint().getY();
+//            double y_st = r0.getY() + r_y.getX() * initialLine.getStartPoint().getX() + r_y.getY() * initialLine.getStartPoint().getY();
+//            double x_en = r0.getX() + r_x.getX() * initialLine.getEndPoint().getX() + r_x.getY() * initialLine.getEndPoint().getY();
+//            double y_en = r0.getY() + r_y.getX() * initialLine.getEndPoint().getX() + r_y.getY() * initialLine.getEndPoint().getY();
+//            transformedLines.add(new Line(new Point(x_st, y_st), new Point(x_en, y_en), initialLine.getTypeOfLine()));
+//        }
+//        return transformedLines;
+//    }
+//
+//    public void executeZoom(){
+//        double maxX = findMaxValue("x", points);
+//        double maxY = findMaxValue("y", points);
+//        double coefByX = maxX / (getWidth() / 2);
+//        double coefByY = maxY / (getHeight() / 2);
+//        performAffineTransformation(new Point(x0, y0), new Point(coefByX, y0), new Point(x0, coefByY));
+//    }
+
+//    private double scaleBetween(double value, double minAllowed, double maxAllowed, double min, double max){
+//        return (maxAllowed - minAllowed) * (value - min) / (max - min) + minAllowed;
+//    }
+//
+//    public void zoom(){
+//        double maxX = findMaxValue("x", points);
+//        double maxY = findMaxValue("y", points);
+//        double minX = findMinValue("x", points);
+//        double minY = findMinValue("y", points);
+//
+//        for (Point point: points){
+//            point.setX(scaleBetween(point.getX(), - getWidth() / 2, getWidth() / 2, minX, maxX));
+//            point.setY(scaleBetween(point.getY(), - getHeight() / 2, getHeight() / 2, minY, maxY));
+//        }
+//    }
+
+    private double findMaxValue(String field, ArrayList<Point> points){
+        double max = 0;
+        for (Point point: points){
+            switch (field){
+                case "x":
+                    if(point.getX() > max)
+                        max = point.getX();
+                    break;
+                case "y":
+                    if(point.getY() > max)
+                        max = point.getY();
+                    break;
+            }
+        }
+        return max;
+    }
+
+    private double findMinValue(String field, ArrayList<Point> points) {
+        double minX = points.get(0).getX();
+        double minY = points.get(0).getY();
+        double min = 0;
+        for (int i = 1; i < points.size(); i++) {
+            switch (field) {
+                case "x":
+                    if (points.get(i).getX() < minX)
+                        minX = points.get(i).getX();
+                    break;
+                case "y":
+                    if (points.get(i).getY() < minY)
+                        minY = points.get(i).getY();
+                    break;
+            }
+        }
+        switch (field) {
+            case "x":
+                if (points.size() == 1)
+                    min = - minX;
+                else min = minX;
+                break;
+            case "y":
+                if (points.size() == 1)
+                    min = - minY;
+                else min = minY;
+                break;
+        }
+        return min;
+    }
+
+    public double getDistance(Point p1, Point p2) {
+        double a = p1.getX() - p2.getX();
+        double b = p1.getY() - p2.getY();
+        return Math.sqrt(a * a + b * b);
+    }
+
+    public void removeSelectedPoint(){
+        points.remove(selectedPoint);
     }
 }
